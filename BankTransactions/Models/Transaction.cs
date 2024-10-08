c4bc753694aa3c010edc7ac06818f8737961fbc0
Sasanka SG <sasankasg697@gmail.com>
2024-10-08 10:59:16 +0530

    first commit

---
+++ BankTransactions/Models/Transaction.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransactions.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage = "Cannot proceed without Account Number")]
+        [MaxLength(12, ErrorMessage = "Maximum 12 Characters Allowed")]
+        public string AccountNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        [Required]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT Code")]
+        [Required]
+        [MaxLength(11, ErrorMessage = "Maximum 11 Characters Allowed")]
+
+        public string SWIFTCode { get; set; }
+
+        public int Amount { get; set; }
+        [DisplayFormat(DataFormatString ="{0:MMM-dd-yyyy}")]
+        public DateTime Date { get; set; }
+
+    }
+}
